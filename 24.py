ceb7c45a1e87f9719d0820e250250b395acfd7c8
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-27 22:47:57 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,13 @@
+ax=input().split()
+k=int(ax[1])
+c=0
+a=list(map(int,input().split()))
+for i in range(0,len(a)):
+    for j in range(i+1,len(a)):
+        if a[i]+a[j]==k:
+            c=1
+            break
+if c==1:
+    print('YES')
+else:
+    print('NO')
